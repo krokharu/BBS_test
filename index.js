7ad4b202897ed58f018ac0e1ff1405b44e2bb9c0
bunkyo_haru <149672409+fallharu@users.noreply.github.com>
2025-06-17 02:46:56 +0900

    2アップロード

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Firebaseの初期化
 const firebaseApp = firebase.initializeApp({
   // APIキーをコピー＆ペースト
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDj0Q6-lFfLgbaXBsx3LUUo4iWV75u0fJk",
+  authDomain: "csd-c1p41035.firebaseapp.com",
+  projectId: "csd-c1p41035",
+  storageBucket: "csd-c1p41035.firebasestorage.app",
+  messagingSenderId: "747560087526",
+  appId: "1:747560087526:web:ce743657010725697bf027"
 });
 
 const db = firebaseApp.firestore();
@@ -14,12 +14,37 @@ const auth = firebaseApp.auth();
 
 // 送信ボタンがクリックされたときの処理
 const sendData = () => {
+  const message = document.getElementById("message").value;
 
+  // コレクション "csd" にドキュメントを追加
+  db.collection("csd").add({
+    message: message,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()  // 任意でタイムスタンプを追加
+  })
+  .then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+    readData();  // 成功したらデータを再読み込み
+  })
+  .catch((error) => {
+    console.error("Error adding document: ", error);
+  });
 };
 
 // データを表示する処理
 const readData = () => {
-
+  db.collection("csd")
+  .get()
+  .than((data) => {
+  var ol = document.getElementById("list")
+  ol.innerHTML = "";  // リストをクリア
+  for (var i = 0; i < data.docs.length; i++) {
+    var id = data .docs[i].id;
+    var message  data.docs[i].data().message;
+    var li = document.createElement("li");
+    li.innerHTML = message ;
+    ol.appendChild(li);
+  }
+});
 };
 
 // ページ読み込み時に実行する処理
